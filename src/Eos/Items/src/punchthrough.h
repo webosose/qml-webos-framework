d69d5b2f83319f7f68756b34be63b1f1cfe2d72b
Jaeyoon Jung <jaeyoon.jung@lge.com>
2019-02-19 06:30:20 +0900

    Remove undefined method
    Change-Id: I090c18376ea1560509bf180c6c1573c18f743457

--- src/Eos/Items/src/punchthrough.h
+++ src/Eos/Items/src/punchthrough.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018 LG Electronics, Inc.
+// Copyright (c) 2014-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -36,8 +36,6 @@ public:
     void setWindowPunchThroughRect();
 
 public slots:
-    void handleDataChanged();
-
     void setXValue();
     void setYValue();
     void setWidthValue();
