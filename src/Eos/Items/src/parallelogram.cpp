e7672d5071bd4d3b0a98893262186976d52d5cd2
beomjoong.yun <bj.yun@lge.com>
2019-12-23 10:59:04 +0900

    Fix bad comparison of floating-point expressions
    Change-Id: I51abbe17ed3c6d1267684400a65ee14525d2462e

--- src/Eos/Items/src/parallelogram.cpp
+++ src/Eos/Items/src/parallelogram.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018 LG Electronics, Inc.
+// Copyright (c) 2014-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ void Parallelogram::updateOffset() {
 }
 
 qreal Parallelogram::offsetAt(qreal y) const {
-    if(height() == 0)
+    if (qFuzzyCompare(height(), 0.0f))
         return 0.0f;
     return m_offset - (m_offset / height()) * y;
 }
@@ -191,7 +191,7 @@ void Parallelogram::updateSolidMaterial(GeometryNode* node) {
 }
 
 void Parallelogram::setAngle(qreal angle) {
-    if (m_angle == angle)
+    if (qFuzzyCompare(m_angle, angle))
         return;
     m_angle = angle;
     m_geometryDirty = true;
--- src/Eos/Items/src/punchthrough.cpp
+++ src/Eos/Items/src/punchthrough.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018 LG Electronics, Inc.
+// Copyright (c) 2014-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -72,22 +72,22 @@ void PunchThrough::setRegion(const QRectF& region)
 {
     int changed = false;
 
-    if (region.x() != x()) {
+    if (!qFuzzyCompare(region.x(), x())) {
         setX(region.x());
         changed = true;
     }
 
-    if (region.y() != y()) {
+    if (!qFuzzyCompare(region.y(), y())) {
         setY(region.y());
         changed = true;
     }
 
-    if (region.width() != width()) {
+    if (!qFuzzyCompare(region.width(), width())) {
         setWidth(region.width());
         changed = true;
     }
 
-    if (region.height() != height()) {
+    if (!qFuzzyCompare(region.height(), height())) {
         setHeight(region.height());
         changed = true;
     }
