d394f43b35f45d93c82c63117883bc447d7ed9b5
Jungi Byun <jungi.byun@lge.com>
2020-01-07 10:48:23 +0900

    Fix missing assignment operator and copy constructor
    Change-Id: I98c783691948ddcd45d15c382c78cfbc0fabc2f2

--- src/Eos/Items/src/samplergeometry.h
+++ src/Eos/Items/src/samplergeometry.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018 LG Electronics, Inc.
+// Copyright (c) 2014-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -157,6 +157,8 @@ class ComposedNode
 public:
     ComposedNode();
     virtual ~ComposedNode();
+    ComposedNode(const ComposedNode&) = delete;
+    ComposedNode &operator=(const ComposedNode&) = delete;
     GeometryNode* body() { return m_body; };
     GeometryNode* fringe() { return m_fringe; };
     void enableFringe();
